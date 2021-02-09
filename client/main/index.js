e9e318d3635f4ceb1c8fafb835262d6a274ca689
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-02-09 13:49:10 -0500

    add temp landing page

--- client/main/index.js
+++ client/main/index.js
@@ -1,3 +1,18 @@
 import "./style.less";
 
-document.getElementById("main-container").innerHTML = "This is index";
+import * as auth from "../api/auth";
+
+document.getElementById("main-container").innerHTML = "Hello";
+
+// prove of login (temp)
+auth.fetchUserInfo().then(data => {
+    const { success, detail } = data;
+    if (!success) {
+        document.getElementById("main-container").innerHTML = "<a target='_self' href='/auth'>Go to login</i>";
+    } else {
+        document.getElementById("main-container").innerHTML = `Hello ${detail.username}`;
+        console.log(detail);
+    }
+}).catch(err => {
+    alert(err);
+})
