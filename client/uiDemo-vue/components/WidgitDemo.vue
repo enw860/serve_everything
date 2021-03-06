f9ed93079dd378fffdca2327d5e873b2fbcfb302
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-06 13:26:49 -0500

    improve live demo box

--- client/uiDemo-vue/components/WidgitDemo.vue
+++ client/uiDemo-vue/components/WidgitDemo.vue
@@ -2,43 +2,88 @@
 @import "../../controls/style/theme/theme.less";
 
 .widgitDemoControl {
-  width: 100%;
+	width: 100%;
 
-  .widgit {
-    width: 60%;
-    margin-right: 16px;
-    background-color: @grey-010;
-  }
+	.widgit {
+		width: 60%;
+		margin-right: 16px;
+		position: relative;
 
-  .widgitControl {
-    width: 40%;
+		> .Background-color-control {
+			position: absolute;
+			bottom: 0;
+			right: 0;
+		}
 
-    > * > * {
-      margin-left: 8px;
-      margin-right: 8px;
-    }
+		> .Demo-box {
+			width: 100%;
+			height: 100%;
+		}
+	}
 
-    > * > *:not(:last-child) {
-      margin-bottom: 16px;
-    }
-  }
+	.widgitControl {
+		width: 40%;
+
+		> * > * {
+			margin-left: 8px;
+			margin-right: 8px;
+		}
+
+		> * > *:not(:last-child) {
+			margin-bottom: 16px;
+		}
+	}
 }
 </style>
 
 <template>
-  <div class="widgitDemoControl HLayout">
-    <div class="widgit HLayout align-center flow-center">
-      <slot name="widgit"></slot>
-    </div>
-    <div class="widgitControl VLayout">
-      <slot name="control"></slot>
-    </div>
-  </div>
+	<div class="widgitDemoControl HLayout">
+		<div
+			class="widgit VLayout"
+			:style="{ backgroundColor: backgroundColor }"
+		>
+			<LabelWrapper
+				class="Background-color-control"
+				value="Background color:"
+				size="small"
+			>
+				<ColorInput
+					slot="labelContent"
+					:value="backgroundColor"
+					@change="updateBackgoundColor"
+				/>
+			</LabelWrapper>
+
+			<div class="Demo-box HLayout align-center flow-center">
+				<slot name="widgit"></slot>
+			</div>
+		</div>
+		<div class="widgitControl VLayout">
+			<slot name="control"></slot>
+		</div>
+	</div>
 </template>
 
 <script>
+import ColorInput from "../../controls/vue/inputs/ColorInput.vue";
+import LabelWrapper from "../../controls/vue/Wrapper/LabelWrapper.vue";
+
 export default {
-  name: "widgitDemo"
+	name: "widgitDemo",
+	components: {
+		ColorInput,
+		LabelWrapper,
+	},
+	data: function () {
+		return {
+			backgroundColor: "#f4f4f4",
+		};
+	},
+	methods: {
+		updateBackgoundColor: function (event) {
+			this.backgroundColor = event.target.value;
+		},
+	},
 };
 </script>
 
